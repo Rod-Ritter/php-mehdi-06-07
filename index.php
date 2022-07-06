711580c91ae9b50f638bcd7b85ab16ae62bbf32e
Rod-Ritter <rodolphe.ritter@gmail.com>
2022-07-06 10:00:02 +0200

    ajout d'un formulaire, nom, prenom, email

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+require ('includes/functions.php');
+require ('includes/pdo.php');
+
+?>
+
+<form action="" method="get">
+<div>
+    <label for="nom">nom: </label>
+    <input type="text" name="nom" id="nom" required>
+  </div>
+  <div>
+    <label for="prenom">prenon: </label>
+    <input type="text" name="prenon" id="prenon" required>
+  </div>
+  <div>
+    <label for="email">Enter your email: </label>
+    <input type="email" name="email" id="email" required>
+  </div>
+
+</form>
+
